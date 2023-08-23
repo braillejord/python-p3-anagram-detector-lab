f93b96556a7090fc830172c7a5df4db8a836574b
BreElle Wells <breellejordyn@gmail.com>
2023-08-23 15:33:04 -0600

    write program that selects correct anagram from list

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.letters_in_word = sorted([letter for letter in word])
+
+    def match(self, list):
+        anagrams_detected = []
+
+        for word in list:
+            if sorted([letter for letter in word]) == self.letters_in_word:
+                anagrams_detected.append(word)
+
+        return anagrams_detected
